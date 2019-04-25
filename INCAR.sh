aa9a6ed38b209ab3b84b5e88c3f2ae38f0e7f748
ywq1113 <419083353@qq.com>
2019-04-25 16:09:30 +0800

    complete the INCAR.sh file

--- INCAR.sh
+++ INCAR.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 # you can choose which type INCAR file to use
 cat <<EOF
-*************************************
- The following is optional INCAR file
-*************************************
+***************************************
+ The following are optional INCAR files
+***************************************
    1) OPT
    2) NEB
    3) OPT+U
-   4) EXIT
-*************************************
+   4) MORE
+   5) EXIT
+***************************************
 EOF
 read -p "please enter your choice:" option
 case $option in
   1)
-    cp opt INCAR
+    cp /home/wanggc/ywq/scripts/opt INCAR
+     read -p "do you need pbsjob and KPOINTS file(YES/NO):" option2
+      case $option2 in
+       YES)
+        cp /home/wanggc/ywq/scripts/pbsjob pbsjob
+        cp /home/wanggc/ywq/scripts/KPOINTS KPOINTS
+        echo "KPOINTS: 3 3 1, pbsjob: 1 nodes 20 np"
+       ;;
+       NO)
+       echo "Well, I respect your choice, and you only obtain INCAR file from me"
+       ;;
+      esac
    ;;
   2)
-    cp neb INCAR
+    cp /home/wanggc/ywq/scripts/neb INCAR
    ;;
   3)
-    cp opt+U INCAR
+    cp /home/wanggc/ywq/scripts/opt+U INCAR
    ;;
   4)
-    break
+    echo "You could edit your own INCAR file in this path:/home/wanggc/ywq/scripts/"
+   ;;
+  5)
+    echo "Good luck"
 esac
