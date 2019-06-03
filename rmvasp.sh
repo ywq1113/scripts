652a37f40ea59070725fc9a2659f9171ca1dda7c
ywqhoho <40077698+ywq1113@users.noreply.github.com>
2019-06-03 16:00:04 +0800

    Update rmvasp.sh
    Update the option to respond to different situations

--- rmvasp.sh
+++ rmvasp.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
-for i in $(find . -name INCAR); do
+cat <<EOF
+**********************************
+  What do you want to delete
+**********************************
+   1) normal
+   2) only input
+   3) EXIT
+**********************************
+EOF
+read -p "please make your choice:" option
+case $option in
+  1)
+   for i in $(find . -name INCAR); do
    cd $(dirname $i)
-   rm CHG CHGCAR PCDAT REPORT OSZICAR WAVECAR EIGENVAL IBZKPT DOSCAR XDATCAR -f
-   tar zcvf OUTCAR.tar.gz OUTCAR outcar vasprun.xml CONTCAR 
-   rm outcar vasprun.xml   
+   rm CHG CHGCAR PCDAT REPORT WAVECAR EIGENVAL IBZKPT DOSCAR XDATCAR -f
+   tar zcvf OUTCAR.tar.gz OUTCAR outcar vasprun.xml CONTCAR
+   rm outcar vasprun.xml
    cd $OLDPWD;
-done   
+   done
+   ;;
+
+  2)
+   for i in $(find . -name INCAR); do
+   cd $(dirname $i)
+   rm CHG CHGCAR PCDAT REPORT WAVECAR EIGENVAL IBZKPT DOSCAR XDATCAR OSZICAR vasprun.xml -f
+   tar czvf outcar.tar.gz outcar*
+   tar czvf OUTCAR.tar.gz OUTCAR*
+   tar czvf CONTCAR.tar.gz CONTCAR*
+   rm -f `ls CONTCAR* OUTCAR* outcar* | grep -v "tar"`
+   cd $OLDPWD;
+   done
+   ;;
+
+  3)
+   echo "Good luck"
+   ;;
+esac
